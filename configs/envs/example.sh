7e7a5343b3225e3aa8d8e01d8d864394cc6edbee
Laszlo Dobos <dobos@jhu.edu>
2023-08-07 10:05:20 -0400

    Updated init scripts.

--- configs/envs/example.sh
+++ configs/envs/example.sh
@@ -1,7 +1,7 @@
-export PFSSPEC_ROOT="~/project/pfsspec-all"
+export PFSSPEC_ROOT="~/project/ga_pfsspec-all"
 export PFSSPEC_DATA="~/data/pfsspec"
 
-export CMDFIT_ROOT="~/project/pfs_cmdfit"
+export CMDFIT_ROOT="~/project/ga_cmdfit"
 export CMDFIT_DATA="~/data/cmdfit"
 
 export PFS_UTILS="~/project/pfs_utils/python"
@@ -11,9 +11,9 @@ export PFS_ETS_FIBER_ASSIGNER="~/project/ets_fiberalloc"
 export PFS_SPT_OPDB="~/project/spt_operational_database/python"
 export PFS_ISOCHRONES="~/project/pfs_isochrones/python"
 
-export PFS_TARGETING_ROOT="~/project/pfs_targeting"
+export PFS_TARGETING_ROOT="~/project/ga_targeting"
 export PFS_TARGETING_DATA="$CMDFIT_DATA"
-export PFS_TARGETING_TEMP="~/project/pfs_targeting/tmp"
+export PFS_TARGETING_TEMP="~/project/ga_targeting/tmp"
 export PFS_TARGETING_CONDAPATH="~/miniconda3"
 export PFS_TARGETING_CONDAENV="astro-tf211"
 export PFS_TARGETING_PYTHONPATH="$PFS_UTILS:$PFS_ICS_COBRAOPS:$PFS_ICS_COBRA_CHARMER:$PFS_ETS_FIBER_ASSIGNER:$PFS_SPT_OPDB:$PFS_ISOCHRONES"
