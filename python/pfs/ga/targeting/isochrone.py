a3a0891b39b1cdf6edcf2fea065973fecf561e96
Laszlo Dobos <dobos@jhu.edu>
2024-02-18 23:49:20 -0500

    Allow getting isochrones as function of EEP.

--- python/pfs/ga/targeting/isochrone.py
+++ python/pfs/ga/targeting/isochrone.py
@@ -13,10 +13,14 @@ class Isochrone(DiagramValueProvider):
             self.__DM = None
             self.__magnitudes = None
             self.__values = None
+            self.__eep = None
+            self.__M_ini = None
         else:
             self.__DM = orig.__DM
             self.__magnitudes = safe_deep_copy(orig.__magnitudes)
             self.__values = safe_deep_copy(orig.__values)
+            self.__eep = safe_deep_copy(orig.__eep)
+            self.__M_ini = safe_deep_copy(orig.M_ini)
 
     def __get_values(self):
         return ReadOnlyDict(self.__values)
@@ -28,23 +32,44 @@ class Isochrone(DiagramValueProvider):
 
     magnitudes = property(__get_magnitudes)
 
-    def from_isogrid(self, photometry, isogrid: IsoGrid, Fe_H, log_t, M_ini=None, DM=None, name_mappings=None, **kwargs):
+    def __get_M_ini(self):
+        return self.__M_ini
+    
+    def __get_eep(self):
+        return self.__eep
+    
+    EEP = property(__get_eep)
+    
+    M_ini = property(__get_M_ini)
+
+    def from_isogrid(self, photometry, isogrid: IsoGrid, Fe_H, log_t, M_ini=None, EEP=None, DM=None, name_mappings=None, **kwargs):
         """
         Creates an isochrone instance from a an isochrone grid using the cmdfit library.
         """
 
-        # TODO: write a version which takes EEP limits instead of M_ini
+        if M_ini is not None and EEP is not None:
+            raise ValueError('Either M_ini or EEP can be defined.')
 
-        if M_ini is None:
+        if M_ini is None and EEP is None:
             M_ini = np.linspace(0.3, 1.2, 10000)
 
         if DM is not None:
             self.__DM = DM
         
-        iso_M_ini = tf.convert_to_tensor(M_ini, dtype=tf.float64)
-        iso_Fe_H = tf.fill(iso_M_ini.shape, tf.constant(Fe_H, dtype=tf.float64))
-        iso_log_t = tf.fill(iso_M_ini.shape, tf.constant(log_t, dtype=tf.float64))
-
+        # TODO: we could do some fancy broadcasting here
+        if M_ini is not None:
+            iso_M_ini = tf.convert_to_tensor(M_ini, dtype=tf.float64)
+            iso_Fe_H = tf.fill(iso_M_ini.shape, tf.constant(Fe_H, dtype=tf.float64))
+            iso_log_t = tf.fill(iso_M_ini.shape, tf.constant(log_t, dtype=tf.float64))
+        elif EEP is not None:
+            iso_EEP = tf.convert_to_tensor(EEP, dtype=tf.float64)
+            iso_Fe_H = tf.fill(iso_EEP.shape, tf.constant(Fe_H, dtype=tf.float64))
+            iso_log_t = tf.fill(iso_EEP.shape, tf.constant(log_t, dtype=tf.float64))
+        else:
+            raise NotImplementedError()
+        
+        # Figure out what values we need to have all magnitudes of the photometric system
+        # TODO: this could be made into a generic function
         name_mappings = name_mappings or {}
         values = []
         for i, m in enumerate(photometry.magnitudes.values()):
@@ -52,7 +77,22 @@ class Isochrone(DiagramValueProvider):
             if name in isogrid.values:
                 values.append(isogrid.values[name])
 
-        _, _, iso_values, iso_mask = isogrid.interp3d(iso_Fe_H, iso_log_t, iso_M_ini, values=values)
+        if EEP is not None:
+            values.append(isogrid.M_ini)
+
+        # Perform the isochrone interpolation
+        if M_ini is not None:
+            eep, _, iso_values, iso_mask = isogrid.interp3d(iso_Fe_H, iso_log_t, iso_M_ini, values=values)
+        elif EEP is not None:
+            iso_values = isogrid._interp3d_EEP(iso_Fe_H, iso_log_t, iso_EEP, values=values, update_index=True)
+            eep = iso_EEP
+            iso_mask = tf.fill(eep.shape, tf.constant(False))
+            M_ini = iso_values.pop(-1)
+        else:
+            raise NotImplementedError()
+        
+        self.__M_ini = M_ini[~iso_mask]
+        self.__eep = np.array(eep)[~iso_mask]
         
         self.__values = {}
         self.__magnitudes = {}
