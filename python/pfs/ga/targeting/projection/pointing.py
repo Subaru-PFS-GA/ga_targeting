dbe514e4745c918244dfd437fe8ba975da1aceb8
Laszlo Dobos <dobos@jhu.edu>
2024-03-14 12:11:41 -0400

    Removed netflow specific things.

--- python/pfs/ga/targeting/projection/pointing.py
+++ python/pfs/ga/targeting/projection/pointing.py
@@ -4,19 +4,15 @@ from ..util.args import *
 
 # TODO: move this elsewhere
 class Pointing():
-    def __init__(self, *pos, posang=None, obs_time=None, obs_cost=None, exp_time=None, orig=None):
+    def __init__(self, *pos, posang=None, obs_time=None, orig=None):
         if not isinstance(orig, Pointing):
             self.__pos = normalize_pos(*pos)
             self.__posang = normalize_angle(posang, u.degree) or normalize_angle(0)
             self.__obs_time = normalize_time(obs_time or datetime.utcnow())
-            self.__obs_cost = obs_cost
-            self.__exp_time = exp_time or normalize_exp_time(1)
         else:
             self.__pos = normalize_pos(*pos) if pos is not None else orig.__pos
             self.__posang = normalize_angle(posang, u.degree) or orig.__posang
             self.__obs_time = obs_time or orig.__obs_time
-            self.__obs_cost = obs_cost or orig.__obs_cost
-            self.__exp_time = exp_time or orig.__exp_time
 
     def __get_pos(self):
         return self.__pos
@@ -51,19 +47,3 @@ class Pointing():
         self.__obs_time = normalize_time(value)
 
     obs_time = property(__get_obs_time, __set_obs_time)
-
-    def __get_obs_cost(self):
-        return self.__obs_cost
-    
-    def __set_obs_cost(self, value):
-        self.__obs_cost = value
-
-    obs_cost = property(__get_obs_cost, __set_obs_cost)
-
-    def __get_exp_time(self):
-        return self.__exp_time
-    
-    def __set_exp_time(self, value):
-        self.__exp_time = value
-
-    exp_time = property(__get_exp_time, __set_exp_time)
