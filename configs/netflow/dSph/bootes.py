c6c264cb3bd97819271f1665a513da63a7bbef1f
enkirby <enk@astro.caltech.edu>
2024-12-21 14:35:56 -0500

    Added Bootes I

---
+++ configs/netflow/dSph/bootes.py
@@ -0,0 +1,79 @@
+from datetime import datetime, timedelta
+from pfs.ga.targeting.targets.dsph import Fornax
+from pfs.ga.targeting.instrument import SubaruHSC
+
+config = dict(
+    targets = {
+        "dsph": dict(
+            path = "/raid/pfs/data/targeting/dSph/bootesi/bootesi_obs.feather",
+            # reader = None
+            reader_args = dict(),
+            column_map = {'objid': 'targetid'},
+            prefix = "sci",
+            epoch = "J2000.0",
+            catid = 15001,
+            proposalid = "PFS-2024-001",
+            filters = {
+                "g_hsc": dict(
+                    mag = 'obs_hsc_g',
+                    mag_err = 'err_hsc_g',
+                    # flux = "g_hsc_flux",
+                    # flux_err = "g_hsc_flux_err",
+                    # psf_mag = "g_hsc",
+                    # psf_mag_err = "g_hsc_err",
+                    # psf_flux = "g_hsc_flux",
+                    # psf_flux_err = "g_hsc_flux_err",
+                    # fiber_mag = "g_hsc",
+                    # fiber_mag_err = "g_hsc_err",
+                    # fiber_flux = "g_hsc_flux",
+                    # fiber_flux_err = "g_hsc_flux_err",
+                    # total_mag = "g_hsc",
+                    # total_mag_err = "g_hsc_err",
+                    # total_flux = "g_hsc_flux",
+                    # total_flux_err = "g_hsc_flux_err",
+                ),
+                "r_hsc": dict(
+                    mag = 'obs_hsc_r',
+                    mag_err = 'err_hsc_r',
+                ),
+            }
+        ),
+        "sky": dict(
+            path = "/raid/pfs/data/targeting/dSph/bootesi/sky_bootesi.feather",
+            reader_args = dict(),
+            column_map = {
+                'sky_id': 'targetid',
+                'ra': 'RA',
+                'dec': 'Dec',
+            },
+            prefix = "sky"
+        ),
+        "fluxstd": dict(
+            path = "/raid/pfs/data/targeting/dSph/bootesi/fluxstd_bootesi.feather",
+            reader_args = dict(),
+            column_map = {
+                'obj_id': 'targetid',
+                'ra': 'RA',
+                'dec': 'Dec',
+            },
+            prefix = "cal",
+            bands = {
+                b: dict(
+                    filter = f'filter_{b}',                   # Column storing filter names
+                    psf_mag = f'psf_mag_{b}',
+                    psf_mag_err = f'psf_mag_error_{b}',
+                    psf_flux = f'psf_flux_{b}',
+                    psf_flux_err = f'psf_flux_error_{b}',
+                ) for b in 'gri'
+            }
+        ),
+    },
+    # Override the minimum number of calibration targets
+    netflow_options = dict(
+        cobra_groups = {
+            'cal_location': dict(
+                min_targets = 0,
+            )
+        }
+    )
+)
