eefd58a821c8fe24d598a28e169271191acb324b
Laszlo Dobos <dobos@jhu.edu>
2025-05-16 17:45:41 -0400

    Added comment on id prefixes

--- python/pfs/ga/targeting/targets/ids.py
+++ python/pfs/ga/targeting/targets/ids.py
@@ -1,3 +1,8 @@
+# Prefixes used to prefix the target index used internally to identify
+# science targets. These are chosen to be large enough to not overlap with
+# the target indices. Note that these are not targetids or objid used in
+# source databases, those number are always left intact.
+
 ID_PREFIX_BOOTES    = 0x0100000000
 ID_PREFIX_DRACO     = 0x0200000000
 ID_PREFIX_FORNAX    = 0x0300000000
@@ -8,3 +13,5 @@ ID_PREFIX_URSAMINOR = 0x0600000000
 ID_PREFIX_M31       = 0x1000000000
 
 ID_PREFIX_OD        = 0x8000000000
+
+ID_PREFIX_CROSSCALIB = 0x10000000000
