1778bd8a4b5675d1e5179b44f9a6ecd2f249e265
Laszlo Dobos <dobos@jhu.edu>
2025-06-12 17:08:14 -0400

    Updated driver script.

--- scripts/ssp_202506_outerdisk.sh
+++ scripts/ssp_202506_outerdisk.sh
@@ -3,7 +3,7 @@
 set -e
 
 PREFIX=SSP
-VERSION="001"
+VERSION="002"
 
 NVISITS=6
 NFRAMES=2
@@ -22,7 +22,8 @@ EXTRA_OPTIONS=""
 # for FIELD in l90_bm28; do
 # for FIELD in l90_bm29; do
 # for FIELD in l90_b25; do
-for FIELD in l90_b27; do
+# for FIELD in l90_b27; do
+for FIELD in l90_b16; do
 
     FIELD_DIR=$PFS_TARGETING_DATA/data/targeting/MW/outerdisk_${FIELD}_${PREFIX}
     IMPORT_DIR=${FIELD_DIR}/import/outerdisk_${FIELD}_${PREFIX}_${VERSION}
