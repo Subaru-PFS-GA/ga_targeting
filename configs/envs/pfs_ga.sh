d56b5dc6179d1a53cf1c7af556ee3c5eb447b03a
enkirby <enk@astro.caltech.edu>
2025-08-12 16:59:26 -0400

    Environment file for stravinsky

---
+++ configs/envs/pfs_ga.sh
@@ -0,0 +1,22 @@
+export PFSSPEC_ROOT="/raid/pfs/ga_pfsspec-all"
+export PFSSPEC_DATA="/raid/pfs/data/pfsspec"
+
+export CMDFIT_ROOT="/raid/pfs/ga_cmdfit"
+export CMDFIT_DATA="/raid/pfs/data/cmdfit"
+
+export PFS_UTILS="/raid/pfs/pfs_utils/python"
+export PFS_INSTDATA="/raid/pfs/pfs_instdata/python"
+export PFS_ICS_COBRAOPS="/raid/pfs/ics_cobraOps/python"
+export PFS_ICS_COBRA_CHARMER="/raid/pfs/ics_cobraCharmer/python"
+export PFS_ICS_FPSACTOR="/raid/pfs/ics_fpsActor/python"
+export PFS_ETS_FIBER_ASSIGNER="/raid/pfs/ets_fiberalloc"
+export PFS_SPT_OPDB="/raid/pfs/spt_operational_database/python"
+export PFS_ISOCHRONES="/raid/pfs/ga_isochrones/python"
+
+export PFS_TARGETING_ROOT="/raid/pfs/ga_targeting"
+export PFS_TARGETING_DATA="/raid/pfs/data/targeting"
+export PFS_TARGETING_TEMP="/raid/pfs/ga_targeting/tmp"
+export PFS_TARGETING_CONDAPATH="/raid/anaconda3"
+export PFS_TARGETING_CONDAENV="pfs_ga"
+export PFS_TARGETING_PYTHONPATH="$PFS_UTILS:$PFS_INSTDATA:$PFS_ICS_COBRAOPS:$PFS_ICS_COBRA_CHARMER:$PFS_ICS_FPSACTOR:$PFS_ETS_FIBER_ASSIGNER:$PFS_SPT_OPDB:$PFS_ISOCHRONES"
+
