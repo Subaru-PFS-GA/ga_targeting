3ac14a7e8a54c18e208aa1e10ace59dc31ec63d8
enkirby <ekirby@nd.edu>
2025-09-01 12:59:44 -0400

    Mature draft of first M31 SSP pointings

--- configs/sample/SSP/m31/m31.py
+++ configs/sample/SSP/m31/m31.py
@@ -2,7 +2,7 @@ import numpy as np
 
 config = dict(
     obs_path = "$PFS_TARGETING_DATA/data/targeting/m31/m31_hsc_catalog_for_pfs_mlprob.csv",
-    pmap_path = "$PFS_TARGETING_DATA/data/targeting/m31/m31_PFS_22_SSP/pmap/m31_PFS_22_SSP_001/",
+    pmap_path = "$PFS_TARGETING_DATA/data/targeting/m31/m31_sector_0_SSP/pmap/m31_sector_0_SSP_001",
     isochrones_path = "$CMDFIT_DATA/isochrones/dartmouth/import/afep0_cfht_sdss_hsc",
     cut_nb = True,
     keep_blue = False,
