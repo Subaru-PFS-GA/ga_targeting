f494719981b2504918374a11484422000ad0b4a7
Laszlo Dobos <dobos@jhu.edu>
2025-12-16 16:13:18 -0500

    Added script for 2026-01 for sextant.

---
+++ scripts/ssp_202601_sextans.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -e
+
+PREFIX=SSP
+VERSION=001
+
+FIELD=sextans
+STAGES="0 1 2 3"        # TODO: update stages as needed, depending on number of pointings
+
+NVISITS=6
+NFRAMES=2
+EXP_TIME=1800
+OBS_TIME="2026-01-12T10:00:00"          # Midnight UTC
+OBS_RUN="2026-01"
+PROPOSAL_ID="S25B-OT02"
+INPUT_CATALOG_ID="10092"
+
+# EXTRA_OPTIONS="--skip-notebooks"
+EXTRA_OPTIONS=""
+
+FIELD_DIR=$PFS_TARGETING_DATA/data/targeting/dSph/${FIELD}_${PREFIX}
+
+PMAP_DIR=${FIELD_DIR}/pmap/${FIELD}_nb
+SAMPLE_DIR=${FIELD_DIR}/sample/${FIELD}_${PREFIX}_${VERSION}
+IMPORT_DIR=${FIELD_DIR}/import/${FIELD}_${PREFIX}_${VERSION}
+EXPORT_DIR=${FIELD_DIR}/export/${FIELD}_${PREFIX}_${VERSION}
+
+# if [ ! -d "$PMAP_DIR" ]; then
+#     ga-pmap --dsph ${FIELD} \
+#         --config ./configs/pmap/SSP/dSph/${FIELD}.py \
+#         --out $PMAP_DIR \
+#         ${EXTRA_OPTIONS}
+# fi
+
+# if [ ! -d "$SAMPLE_DIR" ]; then
+#     ga-sample --dsph ${FIELD} \
+#         --config ./configs/sample/SSP/dSph/${FIELD}.py \
+#         --out $SAMPLE_DIR \
+#         ${EXTRA_OPTIONS}
+# fi
+
+# NOTE: input files to ga-import are listed in the config file
+
+if [ ! -d "$IMPORT_DIR" ]; then
+    ga-import --dsph ${FIELD} \
+        --config \
+            ./configs/netflow/${PREFIX}/dSph/_common.py \
+            ./configs/netflow/${PREFIX}/dSph/${FIELD}.py \
+        --exp-time 1800 \
+        --out $IMPORT_DIR \
+        ${EXTRA_OPTIONS}
+fi
+
+indir=$IMPORT_DIR
+for stage in $STAGES; do
+    outdir=${FIELD_DIR}/netflow/${PREFIX}/${FIELD}_${NVISITS}_${stage}_${VERSION}
+    if [ ! -d "$outdir" ]; then
+        ga-netflow --dsph ${FIELD} \
+            --config \
+                ./configs/netflow/${PREFIX}/dSph/_common.py \
+                ./configs/netflow/${PREFIX}/dSph/${FIELD}.py \
+            --stage ${stage} \
+            --nvisits ${NVISITS} \
+            --exp-time ${EXP_TIME} \
+            --obs-time "${OBS_TIME}" \
+            --in $indir \
+            --out $outdir \
+            ${EXTRA_OPTIONS}
+    fi
+    indir=$outdir
+done
+
+if [ ! -d "$EXPORT_DIR" ]; then
+    ga-export \
+        --in ${FIELD_DIR}/netflow/${PREFIX}/${FIELD}_${NVISITS}_?_${VERSION} \
+        --out ${EXPORT_DIR} \
+        --input-catalog-id $INPUT_CATALOG_ID \
+        --proposal-id $PROPOSAL_ID \
+        --nframes $NFRAMES \
+        --obs-run "$OBS_RUN" \
+        ${EXTRA_OPTIONS}
+fi
