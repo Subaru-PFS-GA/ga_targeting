997a8f87ce7a6f27d142cf02c4c16b0d6bfb2907
Laszlo Dobos <dobos@jhu.edu>
2025-12-24 11:35:24 -0500

    Finalized Sextans configs.

--- configs/netflow/SSP/dSph/sextans.py
+++ configs/netflow/SSP/dSph/sextans.py
@@ -1,44 +1,180 @@
-from datetime import datetime, timedelta
-from pfs.ga.targeting.targets.dsph import Fornax
-from pfs.ga.targeting.instrument import SubaruHSC
+PROPOSALID = 'S25B-OT02'
+CATID_SKY_GAIA = 1006
+CATID_SKY_PS1 = 1007
+CATID_FLUXSTD = 3006
+CATID_SCIENCE_CO = 10091
+CATID_SCIENCE_GA = 10092
+CATID_SCIENCE_GE = 10093
 
-DATA_DIR = '/datascope/subaru'
+extra_columns = {
+    'proposalid': dict(
+        constant = PROPOSALID,
+        dtype = 'string',
+    ),
+    'obcode': dict(
+        pattern = "SSP_GA_S25A_{field}_{target_list}_{{targetid:d}}_{resolution}",
+        dtype = 'string'
+    )
+}
 
 config = dict(
+    # Override the minimum number of calibration targets
+    netflow_options = dict(
+        cobra_groups = {
+            'cal_location': dict(
+                min_targets = 1,
+                max_targets = 12,
+            )
+        },
+        target_classes = {
+            # 'sky': dict(
+            #     prefix = 'sky',
+            #     min_targets = 400,
+            #     max_targets = 420,
+            # ),
+            # 'cal': dict(
+            #     prefix = 'cal',
+            #     min_targets = 200,
+            #     max_targets = 300,
+            # ),
+            # Very likely members on the RGB
+            "sci_P0": {
+                "prefix": "sci",
+                # "non_observation_cost": 1000,
+                "non_observation_cost": 1000,
+                "partial_observation_cost": 100000.0
+            },
+            # Bright p_member > 0.7
+            "sci_P1": {
+                "prefix": "sci",
+                # "non_observation_cost": 775,
+                "non_observation_cost": 1000,
+                "partial_observation_cost": 100000.0
+            },
+            # Bright p_member > 0.0
+            "sci_P2": {
+                "prefix": "sci",
+                "non_observation_cost": 1000,
+                "partial_observation_cost": 100000.0
+            },
+            # BHB + AGB + ToRGB
+            "sci_P3": {
+                "prefix": "sci",
+                "non_observation_cost": 465,
+                "partial_observation_cost": 100000.0
+            },
+            # Blue Stragglers
+            "sci_P4": {
+                "prefix": "sci",
+                "non_observation_cost": 360,
+                "partial_observation_cost": 100000.0
+            },
+            # Anc 0, Pace, Sestito
+            "sci_P5": {
+                "prefix": "sci",
+                "non_observation_cost": 600,
+                "partial_observation_cost": 100000.0
+            },
+            # Anc 1
+            "sci_P6": {
+                "prefix": "sci",
+                "non_observation_cost": 450,
+                "partial_observation_cost": 100000.0
+            },
+            # Anc 2
+            "sci_P7": {
+                "prefix": "sci",
+                "non_observation_cost": 350,
+                "partial_observation_cost": 100000.0
+            },
+            # Faint dSph members
+            "sci_P8": {
+                "prefix": "sci",
+                "non_observation_cost": 130,
+                "partial_observation_cost": 100000.0
+            },
+            # Fillers
+            "sci_P9": {
+                "prefix": "sci",
+                "non_observation_cost": 10,
+                "partial_observation_cost": 100000.0
+            }
+        },
+    ),
     targets = {
-        "dsph": dict(
-            path = f"{DATA_DIR}/data/targeting/dSph/bootesi/bootesi_obs.feather",
+        "hsc": dict(
+            path = f"$PFS_TARGETING_DATA/data/targeting/dSph/sextans/sample/SSP/sextans_SSP_001/hsc_sex_priorities.feather",
             # reader = None
             reader_args = dict(),
             column_map = {'objid': 'targetid'},
             prefix = "sci",
-            epoch = "J2016.0",
-            catid = 15001,
-            extra_columns = {
-                'proposalid': dict(
-                    pattern = "SSP_GA_ENG_{obs_time:%Y%m%d}_{name}",
-                    dtype = 'string',
-                ),
-                'obcode': dict(
-                    pattern = "SSP_GA_ENG_{obs_time:%Y%m%d}_{name}_{{targetid:d}}_{resolution}",
-                    dtype = 'string'
-                )
-            },
+            frame='icrs',
+            epoch = 2016.0,
+            catid = CATID_SCIENCE_GA,
+            extra_columns = extra_columns,
             photometry = dict(
                 filters = {
                     "g_hsc": dict(
                         mag = 'obs_hsc_g',
                         mag_err = 'err_hsc_g',
                     ),
-                    "r_hsc": dict(
-                        mag = 'obs_hsc_r',
-                        mag_err = 'err_hsc_r',
+                    "i2_hsc": dict(
+                        mag = 'obs_hsc_i2',
+                        mag_err = 'err_hsc_i2',
+                    ),
+                    "nb515_hsc": dict(
+                        mag = 'obs_hsc_nb515',
+                        mag_err = 'err_hsc_nb515',
                     ),
                 }
             ),
         ),
+        # Roman
+        "anc": dict(
+            path = "$PFS_TARGETING_DATA/data/targeting/dSph/sextans/Gerasimov_ancillary_sextans.feather",
+            # reader = None
+            reader_args = dict(),
+            column_map = {'objid': 'targetid'},
+            value_map = {
+                'priority': {
+                    0: 5,
+                    1: 6,
+                    2: 7
+                }
+            },
+            prefix = "sci",
+            frame= 'icrs',
+            epoch = 2016.0,
+            catid = CATID_SCIENCE_GA,
+            extra_columns = extra_columns,
+            photometry = dict(
+                filters = {
+                    "g_ps1": dict(
+                        mag = 'obs_ps_g',
+                        mag_err = 'err_ps_g',
+                    ),
+                    "r_ps1": dict(
+                        mag = 'obs_ps_r',
+                        mag_err = 'err_ps_r',
+                    ),
+                    "i_ps1": dict(
+                        mag = 'obs_ps_i',
+                        mag_err = 'err_ps_i',
+                    ),
+                    "z_ps1": dict(
+                        mag = 'obs_ps_z',
+                        mag_err = 'err_ps_z',
+                    ),
+                },
+                limits = {
+                    'ps1_g': [16, 23],
+                    'ps1_i': [16, 23],
+                }
+            )
+        ),
+        # Miho
         "sky": dict(
-            path = f"{DATA_DIR}/data/targeting/dSph/bootesi/sky_bootesi.feather",
+            path = "$PFS_TARGETING_DATA/data/targeting/dSph/sextans/sky_sextans.feather",
             reader_args = dict(),
             column_map = {
                 'sky_id': 'targetid',
@@ -46,55 +182,43 @@ config = dict(
                 'dec': 'Dec',
             },
             prefix = "sky",
+            catid = CATID_SKY_PS1,
+            extra_columns = extra_columns,
         ),
+        # Miho
         "fluxstd": dict(
-            path = f"{DATA_DIR}/data/targeting/dSph/bootesi/fluxstd_bootesi.feather",
+            path = "$PFS_TARGETING_DATA/data/targeting/dSph/sextans/fluxstd_sextans.feather",
             reader_args = dict(),
             column_map = {
-                'obj_id': 'targetid',
+                'fluxstd_id': 'targetid',
                 'ra': 'RA',
                 'dec': 'Dec',
+                'parallax_error': 'err_parallax',
+                'pmra_error': 'err_pmra',
+                'pmdec_error': 'err_pmdec',
             },
+            # log_prob_star is missing from fluxstd file
+            # mask = 'lambda df: df["prob_f_star"] > 0.5',
             prefix = "cal",
-            extra_columns = {
-                'proposalid': dict(
-                    pattern = "SSP_GA_ENG_{obs_time:%Y%m%d}_{name}",
-                    dtype = 'string',
-                ),
-                'obcode': dict(
-                    pattern = "SSP_GA_ENG_{obs_time:%Y%m%d}_{name}_{{targetid:d}}_{resolution}",
-                    dtype = 'string'
-                )
-            },
+            frame = 'icrs',
+            epoch = 2016.0,
+            catid = CATID_FLUXSTD,
+            extra_columns = extra_columns,
             photometry = dict(
                 bands = {
                     b: dict(
-                        filter = f'filter_{b}',                   # Column storing filter names
-                        psf_mag = f'psf_mag_{b}',
-                        psf_mag_err = f'psf_mag_error_{b}',
+                        filter = f'filter_{b}',                     # Column storing filter names
+                        # psf_mag = f'psf_mag_{b}',                 # All None in data file
+                        # psf_mag_err = f'psf_mag_error_{b}',       # All None in data file
                         psf_flux = f'psf_flux_{b}',
                         psf_flux_err = f'psf_flux_error_{b}',
-                    ) for b in 'gri'
+                    ) for b in 'grizy'
                 },
                 limits = {
-                    'ps1_g': [17, 19],
+                    'ps1_g': [16, 19.5],
+                    'ps1_g-ps1_r': [0.25, 0.4],
                 }
-            ),
+            )
         ),
     },
-    # Override the minimum number of calibration targets
-    netflow_options = dict(
-        target_classes = {
-            'cal': dict(
-                min_targets = 200,
-                max_targets = 300,
-            )
-        },
-        cobra_groups = {
-            'cal_location': dict(
-                min_targets = 15,
-                max_targets = 20,
-            )
-        }
-    )
 )
